908c58dcec5baaff0e51f01cd4d7a9d9bd73206a
Miłosz Nowaczyk <milosz.nowaczyk@protonmail.com>
2022-07-27 21:47:21 +0200

    Init repo

---
+++ src/main/kotlin/su/nextserver/anotherreactnativesvgparser/anotherreactnativesvgparserserver/AnotherreactnativesvgparserserverApplication.kt
@@ -0,0 +1,11 @@
+package su.nextserver.anotherreactnativesvgparser.anotherreactnativesvgparserserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnotherreactnativesvgparserserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<AnotherreactnativesvgparserserverApplication>(*args)
+}
---
+++ src/test/kotlin/su/nextserver/anotherreactnativesvgparser/anotherreactnativesvgparserserver/AnotherreactnativesvgparserserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package su.nextserver.anotherreactnativesvgparser.anotherreactnativesvgparserserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnotherreactnativesvgparserserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
